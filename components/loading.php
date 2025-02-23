0f41d36bdc457a206e4508feffd73a2fb6bd6447
jorieltejada28 <jorieltejada84@gmail.com>
2025-02-23 18:36:07 +0800

    loading screen

--- components/loading.php
+++ components/loading.php
@@ -1,5 +1,5 @@
 <!-- Loading Screen -->
 <div class="spinner-container text-center">
     <div class="spinner"></div>
-    <div class="loading-text">Loading...</div>
+    <div class="loading-text">Loading</div>
 </div>
